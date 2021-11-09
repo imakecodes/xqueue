d55c7c6088a9375448168008a3368b5381715844
Michel Wilhelm <michel.wilhelm@loadsmart.com>
2021-11-09 19:38:18 -0300

    update

--- app/database.py
+++ app/database.py
@@ -1,13 +1,20 @@
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://xqueue:xqueue@postgres/xqueue")
+from app.config import settings
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.db.SQLALCHEMY_DATABASE_URI,
+    # required for sqlite
+    connect_args={"check_same_thread": False},
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def init_db(db_session: Session):
+    Base.metadata.create_all(engine)
